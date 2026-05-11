63178ed69a1a3e25b1eec97605830c2ebcdca7eb
Revanth Yadav <dhandikarevanth@gmail.com>
2026-05-11 13:24:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,3 +128,36 @@ template.addEventListener("change", function () {
   }
 
 });
+function openLogin() {
+
+  document.getElementById("loginModal").style.display = "block";
+}
+
+function closeLogin() {
+
+  document.getElementById("loginModal").style.display = "none";
+}
+
+function openSignup() {
+
+  document.getElementById("signupModal").style.display = "block";
+}
+
+function closeSignup() {
+
+  document.getElementById("signupModal").style.display = "none";
+}
+
+function switchToSignup() {
+
+  closeLogin();
+
+  openSignup();
+}
+
+function switchToLogin() {
+
+  closeSignup();
+
+  openLogin();
+}
